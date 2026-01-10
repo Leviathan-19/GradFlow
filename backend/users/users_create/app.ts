3ae081273c89b85fd6f73e05c264e0e728eb475a
Leviathan-19 <msnarvaez19@hotmail.com>
2026-01-10 14:34:59 -0500

    feat: :sparkles: update users_create microservice from backend/add swagger test

---
+++ backend/users/users_create/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import usersRoutes from './users.routes';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerOptions } from './swagger';
+
+const app = express();
+
+app.use(express.json());
+
+// Rutas
+app.use('/api', usersRoutes);
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+
+export default app;
