b6262f9920954898ba1798270ca7b7b0a79fc5ae
Leviathan-19 <msnarvaez19@hotmail.com>
2026-01-11 00:27:18 -0500

    feat: :white_check_mark: re-structurate frontend with Vite + React install dependencies

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
