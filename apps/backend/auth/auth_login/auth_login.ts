aece70825b3a835bdcbaf49889583d9e82d1520f
Leviathan-19 <msnarvaez19@hotmail.com>
2026-01-11 21:31:08 -0500

    feat: :white_check_mark: add JWT to login microservice, testing correctly

--- apps/backend/auth/auth_login/auth_login.ts
+++ apps/backend/auth/auth_login/auth_login.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { pool } from "./db";
 import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 export const authLogin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -33,20 +34,31 @@ export const authLogin = async (req: Request, res: Response) => {
     }
 
     const user = result.rows[0];
+
     if (!user.status) {
       return res.status(403).json({ message: "User is inactive" });
     }
-    console.log("EMAIL INPUT:", email);
-    console.log("PASSWORD INPUT:", password);
-    console.log("HASH FROM DB:", user.password);
 
     const isValidPassword = await bcrypt.compare(password, user.password);
 
     if (!isValidPassword) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        rol: user.rol_id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN || "1h",
+      } as jwt.SignOptions
+    );
+
     res.status(200).json({
       message: "Login successful",
+      token,
       user: {
         id: user.id,
         name: user.name,
