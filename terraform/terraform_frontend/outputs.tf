848f6f42471a3ae87ae4027ddca834f7c07d9578
Leviathan-19 <msnarvaez19@hotmail.com>
2026-01-16 12:49:51 -0500

    feat: :bricks: Split terraform infrastructure by domain (terraform_users, terraform_auth, terraform_docs, terraform_edge, terraform_frontend)
    each branch, direct each different count and responsabilities

---
+++ terraform/terraform_frontend/outputs.tf
@@ -0,0 +1,24 @@
+output "loadbalancer_dns" {
+  description = "public DNS Load balancer"
+  value       = aws_lb.app.dns_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "target_group_arn" {
+  description = "ARN del Target Group usado por el ALB"
+  value       = aws_lb_target_group.app.arn
+}
+
+output "asg_name" {
+  description = "Nombre del Auto Scaling Group"
+  value       = aws_autoscaling_group.app.name
+}
+
+
